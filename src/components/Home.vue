25dc698edc55d593b70c71ebd494289e5cc87ec9
Sophie <sophie4ngo@gmail.com>
2019-11-25 05:48:20 -0800

    Add Img

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,13 +3,20 @@
     <b-container class="bv-example-row">
       <b-row class="columns">
         <b-col>
-          <h2>Info</h2>
+
+          <h2>MICHAEL & SOPHIE</h2> <h2>July 10th 2020 @ 5:00 pm </h2> <h2> Emeryville </h2> 
+          <h2 id="subtitle"><u>Info</u></h2>
            We are going to have our wedding ceremony by the ocean next to a nice venue 
            called HK East Ocean Seafood Restaurant in Emeryville California which is 8 miles 
            from San Francisco. The address is 3199 Powell St, Emeryville, CA 94608, US. 
-           Delicious food and alcohol such as lobster, wine and pink gin will be served, and beer is on tap.</b-col>
-        <b-col> <h3>MICHAEL & SOPHIE</h3> <h3>July 10th 2020 @ 5:00 pm </h3> <h3> Emeryville </h3> </b-col>
-        <b-col><h2>Preparation</h2><b>Dresscode</b>: cocktail attire but light, <b>Venue</b>: Please procceed to the park area next to the venue first for the ceremony and be back at 7:00 pm for the reception <b>Accomodation</b>: Airbnb or Hampton Inn Oakland Hotel </b-col>
+           Delicious food and alcohol such as lobster, wine and pink gin will be served, and beer is on tap.
+
+           <h2 id="subtitle"><u>Preparation</u></h2>Dresscode: cocktail attire but light, Venue: Please procceed to the park area next to the venue first for the ceremony and be back at 7:00 pm for the reception Accomodation: Airbnb or Hampton Inn Oakland Hotel
+        </b-col>
+        <b-col class="portrait"> 
+          <img src="https://wallpapercave.com/wp/wp2553950.jpg">
+        </b-col>
+
       </b-row>
     </b-container>
   </div>
@@ -32,8 +39,18 @@ export default {
 
  }
 
- h3 {
+ h2 {
   text-align: center;
  }
+
+ .portrait img {
+    height: 820px;
+    width: 600px;
+    margin-bottom: 200px;
+}
+
+#subtitle{
+  margin-top: 40px;
+}
 
 </style>
